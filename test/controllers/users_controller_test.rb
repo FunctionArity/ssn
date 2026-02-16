2bd661096394c84162de09b02454c0b7d3a6cb1f
Pablo Rodriguez <pablorodriguez.ar@gmail.com>
2026-02-16 17:15:00 -0300

    add error css class

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -20,7 +20,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should create user" do
     assert_difference("User.count") do
-      post users_url, params: { user: { email: Faker::Internet.email, first_name: @user.first_name, last_name: @user.last_name } }
+      post users_url, params: { user: { email: Faker::Internet.email, first_name: @user.first_name, last_name: @user.last_name, password: "password123", password_confirmation: "password123" } }
     end
 
     assert_redirected_to user_url(User.last)
@@ -37,7 +37,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update user" do
-    patch user_url(@user), params: { user: { email: @user.email, first_name: @user.first_name, last_name: @user.last_name } }
+    patch user_url(@user), params: { user: { email: @user.email, first_name: @user.first_name, last_name: @user.last_name, password: "password123", password_confirmation: "password123" } }
     assert_redirected_to user_url(@user)
   end
 
