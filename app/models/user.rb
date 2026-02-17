776a2fd6ffe9be6dd073ace3d23cf692e33b5466
Pablo Rodriguez <pablorodriguez.ar@gmail.com>
2026-02-17 08:23:12 -0300

    add user's role

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum :role, { guardian: 0, vocal: 1, priest: 2 }, default: :guardian
+
+  def role_badge_class
+    case role
+    when "guardian" then "badge_green"
+    when "vocal"    then "badge_red"
+    when "priest"   then "badge_purple"
+    end
+  end
 end
