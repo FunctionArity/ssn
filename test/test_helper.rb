a3ce688e8499c5f03e27a3c24ebfda19f82b624d
Pablo Rodriguez <pablorodriguez.ar@gmail.com>
2026-03-03 00:52:47 -0300

    add test coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,6 @@
+require "simplecov"
+SimpleCov.start "rails"
+
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
@@ -6,6 +9,9 @@ module ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers
     parallelize(workers: :number_of_processors)
+
+    parallelize_setup { |worker| SimpleCov.command_name "#{SimpleCov.command_name}-#{worker}" }
+    parallelize_teardown { |_worker| SimpleCov.result }
 
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
