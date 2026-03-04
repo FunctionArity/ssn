469a237356f65f8f18f59b2610534552e2172684
Pablo Rodriguez <pablorodriguez.ar@gmail.com>
2026-03-04 10:31:13 -0300

    small UI changes and fix test

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -21,11 +21,10 @@ class UsersTest < ApplicationSystemTestCase
     visit users_url
     click_on I18n.t("users.index.new_user")
 
-    fill_in User.human_attribute_name(:email), with: Faker::Internet.email
     fill_in User.human_attribute_name(:first_name), with: @user.first_name
     fill_in User.human_attribute_name(:last_name), with: @user.last_name
-    fill_in User.human_attribute_name(:password), with: "password123", match: :first
-    fill_in User.human_attribute_name(:password_confirmation), with: "password123"
+    fill_in User.human_attribute_name(:email), with: Faker::Internet.email
+    fill_in User.human_attribute_name(:phone), with: "1234567899"
     click_on I18n.t("helpers.submit.create", model: User.model_name.human)
 
     assert_text I18n.t("users.notices.created")
@@ -34,7 +33,7 @@ class UsersTest < ApplicationSystemTestCase
 
   test "should update User" do
     visit user_url(@user)
-    click_on I18n.t("users.show.edit"), match: :first
+    click_on I18n.t("edit"), match: :first
 
     fill_in User.human_attribute_name(:email), with: Faker::Internet.email
     fill_in User.human_attribute_name(:first_name), with: @user.first_name
@@ -47,7 +46,8 @@ class UsersTest < ApplicationSystemTestCase
 
   test "should destroy User" do
     visit user_url(users(:agustin))
-    click_on I18n.t("users.show.destroy"), match: :first
+    click_on I18n.t("destroy"), match: :first
+    click_on "Aceptar"
 
     assert_text I18n.t("users.notices.destroyed")
   end
