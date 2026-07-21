8d0ce98d899e1fbe1c772071ae68e78e55b467c0
Pablo Rodriguez <pablorodriguez.ar@gmail.com>
2026-07-21 19:34:49 -0300

    update style on sedes public

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,7 @@ Rails.application.routes.draw do
   namespace :doc do
     resources :headquarters, only: %i[ index ]
   end
+  get "sedes", to: "doc/headquarters#index", as: :sedes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
