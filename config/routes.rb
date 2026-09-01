a265bcd17416db934ae46facf05ad653c364b45d
Pablo Rodriguez <pablorodriguez.ar@gmail.com>
2026-09-01 12:12:01 -0300

    remove no need mount

--- config/routes.rb
+++ config/routes.rb
@@ -40,9 +40,6 @@ Rails.application.routes.draw do
   get "federacion", to: "doc/federacion#index", as: :federacion
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Mount Action Cable so Turbo Stream broadcasts can reach connected clients over WebSockets.
-  mount ActionCable.server => "/cable"
-
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
